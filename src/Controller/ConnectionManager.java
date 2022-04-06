d756f327370e06ffaac317f39d07bf924382dd77
synx <synx@WIN-1JN8QQ6FA85>
2022-04-06 21:18:42 +0700

    first commit

---
+++ src/Controller/ConnectionManager.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author synx
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionManager {
+    private Connection con;
+    private String Driver = "com.mysql.jdbc.Driver";
+    private String url = "jdbc:mysql://localhost:3306/uts_pbd(p)";
+    private String Username = "root";
+    private String Password = "";
+    
+    public Connection LogOn(){
+        try {
+            Class.forName(Driver).newInstance();
+            con = DriverManager.getConnection(url, Username, Password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+    
+    public void LogOff(){
+        try {
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
